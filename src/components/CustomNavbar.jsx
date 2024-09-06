20b5e656388cda9263b82fb4f1d4d9c76b1dbfc7
Mohammed Khallaf <mohammedkhallaf.dev@gmail.com>
2024-09-06 19:18:36 +0300

    before break

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -6,11 +6,11 @@ function CustomNavbar() {
   return (
     <Navbar bg="dark" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Brand href="#home">Ecommerce</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link href="#pricing">Cart</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
